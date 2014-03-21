be94e6b97e0481891e5c62a734b530dd450d7b70
Aliece <alieceyy@gmail.com>
2014-03-21 10:47:25 +0800

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,6 @@
 $(document).ready(function() {
+    $('#nav_ul li').click(function (e) {
+      $(this).attr('class','active');
+      $("#nav_ul li[class='active']").removeClass("active"); 
+    });
 });
